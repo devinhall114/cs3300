3f8ce2a1252ca9ad472c2122d1bd23ae052476c7
Devin <devinhall114@gmail.com>
2022-05-10 23:08:33 +0000

    Final

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do 
-  describe 'user registration' do 
+  describe 'user registration ' do 
     it "name, email, password and password_You can register if confirmation exists" do 
       user = build(:user) 
       expect(user).to be_valid 
